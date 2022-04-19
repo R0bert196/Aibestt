c8019c9cf0deee82c1c10a6d3284b97975456040
R0bert196 <robertchiriac196@gmail.com>
2022-04-19 10:41:02 +0300

    add css import in index js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">test</header>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold hover:underline">Hello world!</h1>
     </div>
   );
 }
