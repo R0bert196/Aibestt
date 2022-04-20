f332befe5a38d23a774c979e3707ac29539ca60e
alexsincai <alex.sincai@yahoo.co.uk>
2022-04-20 15:45:34 +0300

    login refactored

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,57 +1,84 @@
-import {useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useAtom } from 'jotai';
-import state from '../state';
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAtom } from "jotai";
+import state from "../state";
 
 function LoginForm() {
- const [formData, setFormData] = useState({
-        email: '', // required
-        password: '' // required
-    })
+    const [formData, setFormData] = useState({
+        email: "", // required
+        password: "", // required
+    });
 
-    const [token, setToken] = useAtom(state.token)
+    const [token, setToken] = useAtom(state.token);
 
     let navigate = useNavigate();
 
     function handleSubmit(e) {
-        e.preventDefault()
-        fetch('http://localhost:4000/login', {
-            method: 'POST',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(formData)
+        e.preventDefault();
+        fetch("http://localhost:4000/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
         })
-        .then(res => res.json())
-        .then(data => {
-            setToken(data.accessToken)
-            navigate("/")
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                setToken(data.accessToken);
+                navigate("/");
+            });
     }
 
     function handleChange(e) {
-        setFormData({...formData, [e.target.name] : e.target.value})
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
-
-  return (
-    <div>
-      <form onSubmit={e => handleSubmit(e)} className='center-center border-b border-solid'>
+    return (
         <div>
-          <input className="w-full py-4 rounded-3xl my-4 px-4 border-solid border" defaultValue={formData.email} onChange={e => handleChange(e)} type="email" placeholder="Enter Email Address..." name="email" id="password" />
+            <form
+                onSubmit={handleSubmit}
+                className="center-center border-b border-solid">
+                <div>
+                    <input
+                        className="w-full py-4 rounded-3xl my-4 px-4 border-solid border"
+                        defaultValue={formData.email}
+                        onChange={handleChange}
+                        type="email"
+                        placeholder="Enter Email Address..."
+                        name="email"
+                        id="email"
+                    />
+                </div>
+                <div>
+                    <input
+                        className="w-full py-4 rounded-3xl my-4 px-4 border-solid border"
+                        value={formData.password}
+                        onChange={handleChange}
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        id="password"
+                    />
+                </div>
+                <div>
+                    <input
+                        className=""
+                        type="checkbox"
+                        name="rememebr-me"
+                        id="remember-me"
+                    />
+                    <label className="mx-4" htmlFor="remember-me">
+                        Remember Me
+                    </label>
+                </div>
+                <div>
+                    <button
+                        className="py-2 bg-primary text-white hover:brightness-125 w-full px-4 my-4 rounded-3xl"
+                        type="submit">
+                        Login
+                    </button>
+                </div>
+            </form>
         </div>
-        <div>
-          <input className="w-full py-4 rounded-3xl my-4 px-4 border-solid border" value={formData.password} onChange={e => handleChange(e)} type="password" placeholder="Password" name="password" id="password" />
-        </div>
-        <div>
-          <input className="" type="checkbox" name="rememebr-me" id="remember-me" />
-          <label className="mx-4" htmlFor="remember-me">Remember Me</label>
-        </div>
-        <div>
-          <button className="py-2 bg-primary text-white hover:brightness-125 w-full px-4 my-4 rounded-3xl" type="submit">Login</button>
-        </div>
-      </form>
-    </div>
-  )
+    );
 }
 
-export default LoginForm
+export default LoginForm;
