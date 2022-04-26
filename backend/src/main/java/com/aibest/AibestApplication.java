6841ab0413f29343d636f424d7e1d8a3e8ec0dad
R0bert196 <robertchiriac196@gmail.com>
2022-04-26 10:12:37 +0300

    Initial backend commit, add .gitignore

---
+++ backend/src/main/java/com/aibest/AibestApplication.java
@@ -0,0 +1,13 @@
+package com.aibest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AibestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aibest/AibestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
