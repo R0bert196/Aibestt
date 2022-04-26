108318cfd4a4a9993a12bff33a24bc80ddddce77
gligoravram <gligor_avram@yahoo.com>
2022-04-26 13:14:34 +0300

    add basic elements on the profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -4,8 +4,34 @@ function Profile() {
   return (
         <div>
           <h1>My profile</h1>
+           <div>
+            <img src="" alt="pic"/>
+            <div class="container">
+              <h4><b>John Doe</b></h4>
+              <p> Engineer</p>
+            </div>
+          </div> 
+
           <div>
-            
+            <h3>User settings</h3>
+            <form>
+              <label>
+                Name:
+                <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+
+          <div>
+            <h3>Contact settings</h3>
+            <form>
+              <label>
+                Name:
+                <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
           </div>
         </div>
   )
