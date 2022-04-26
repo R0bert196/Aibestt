388d5919ea75f0217e8e7d2fe854dbaf159f8418
gligoravram <gligor_avram@yahoo.com>
2022-04-26 14:07:27 +0300

    add basic css to the forms on the profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,38 +1,67 @@
+import avatar from '../images/avatar.png'
 
 
 function Profile() {
   return (
-        <div>
-          <h1>My profile</h1>
+    <div className='w-full'>
+        <h1>My profile</h1>
+          <div className="grid grid-cols-2 gap-4 mt-3 ml-5">        
            <div>
-            <img src="" alt="pic"/>
-            <div class="container">
+             <div className="w-10 min-w-[25%]">
+                <img src={avatar} alt="pic"/>
+             </div>            
+            <div >
               <h4><b>John Doe</b></h4>
-              <p> Engineer</p>
+              <p>Engineer</p>
             </div>
           </div> 
 
           <div>
-            <h3>User settings</h3>
-            <form>
+            <h3>Setari cont</h3>
+            <form className='flex flex-col gap-2'>
               <label>
-                Name:
+                Adresa email:
                 <input type="text" name="name" />
               </label>
+              <label>
+                Parola veche:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Parola noua:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Confirmare parola:
+                <input type="text" name="name" />
+              </label>            
               <input type="submit" value="Submit" />
             </form>
           </div>
 
           <div>
-            <h3>Contact settings</h3>
-            <form>
+            <h3>Detalii de contact</h3>
+            <form className='flex flex-col gap-2'>
               <label>
-                Name:
+                Adresa:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Oras:
+                <input type="text" name="name" />
+              </label>              
+              <label>
+                Judet:
+                <input type="text" name="name" />
+              </label>
+              <label>
+                Nr. Telefon:
                 <input type="text" name="name" />
               </label>
               <input type="submit" value="Submit" />
             </form>
           </div>
+        </div>
         </div>
   )
 }
