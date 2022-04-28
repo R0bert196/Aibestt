add15dc0f2d1bf508fa963603b8962e6d7509a14
R0bert196 <robertchiriac196@gmail.com>
2022-04-28 22:02:56 +0300

    Add font size on table header

--- frontend/src/components/Employees.jsx
+++ frontend/src/components/Employees.jsx
@@ -6,6 +6,7 @@ function Employees() {
   return (
     <div className='block mx-4 my-14 px-5 w-full'>
       <div>
+        <h1 className='mb-4 text-3xl'>Empoyees</h1>
         <BasicTable />
       </div>
     </div>
