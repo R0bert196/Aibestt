46641cc3e3a190afded6961a4be686c7529aac0c
R0bert196 <robertchiriac196@gmail.com>
2022-04-28 21:49:06 +0300

    Add mt on table

--- frontend/src/components/Employees.jsx
+++ frontend/src/components/Employees.jsx
@@ -4,7 +4,7 @@ import FileUploadButton from './FileUploadButton'
 
 function Employees() {
   return (
-    <div className='block mx-4 my-10 px-5 w-full'>
+    <div className='block mx-4 my-14 px-5 w-full'>
       <div>
         <BasicTable />
       </div>
