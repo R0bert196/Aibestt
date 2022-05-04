7ea1cd312d22c9d65427612d3a930561f2d5b52e
R0bert196 <robertchiriac196@gmail.com>
2022-05-04 13:06:23 +0300

    Change privateApi from put to get

--- frontend/src/hooks/useRefreshToken.jsx
+++ frontend/src/hooks/useRefreshToken.jsx
@@ -8,15 +8,15 @@ const useRefreshToken = () => {
     const [token, setToken] = useAtom(state.token);
 
     const refresh = async () => {
-        const response = await Api.post('/refresh', { headers: {"Authorization" : `Bearer ${token}`} });
+        const response = await Api.get('/refreshtoken', { headers: {"Authorization" : `Bearer ${token}`} });
         // setAuth(prev => {
         //     // console.log(JSON.stringify(prev));
         //     console.log(response.data.token);
         //     return { ...prev, accessToken: response.data.accessToken }
         // });
-        console.log(response.data);
-        setToken(response.data);
-        return response.data;
+        console.log(response.data.jwtToken);
+        setToken(response.data.jwtToken);
+        return response.data.jwtToken;
     }
     return refresh;
 };
