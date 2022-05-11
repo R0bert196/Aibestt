dad9dbfc0ec788b40318b5091b2154d32505b15b
gligoravram <gligor_avram@yahoo.com>
2022-05-11 19:33:51 +0300

    add email sending capabilities

--- backend/src/main/java/com/aibest/AibestApplication.java
+++ backend/src/main/java/com/aibest/AibestApplication.java
@@ -1,8 +1,12 @@
 package com.aibest;
 
+import com.aibest.services.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 public class AibestApplication {
+	@Autowired
+	EmailSenderService senderService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AibestApplication.class, args);
 	}
@@ -19,5 +26,9 @@ public class AibestApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendEmail("avram.gligor@gmail.com", "test", "test body");
+	}
 
 }
