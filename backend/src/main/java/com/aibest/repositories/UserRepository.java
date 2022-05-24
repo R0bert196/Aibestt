978e9ba374b48e33e53ae4ba5f02c6f3a8543111
gligoravram <gligor_avram@yahoo.com>
2022-05-24 18:19:37 +0300

    rewrite findByEmail function in the userrepo

--- backend/src/main/java/com/aibest/repositories/UserRepository.java
+++ backend/src/main/java/com/aibest/repositories/UserRepository.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
-//    AppUser findByEmail(String email);
-
     AppUser findByEmail(String email);
 
     @Query("SELECT u FROM AppUser u WHERE u.verificationCode = ?1")
